da30db21a214e72639e364d121a8b3fb986c3950
Mudassir Khan <12mudassarkhan@gmail.com>
2025-08-02 13:11:22 +0530

    Edit Update and Delete properties and now enabled

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const app = express();
 const mongoose = require('mongoose');
 const path=require('path');
 const Chat = require('./models/chat.js');
+const methodOverRide = require('method-override');
 
 app.set("views",path.join(__dirname,"views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname,"public")));
 app.use(express.urlencoded({extended:true}));
+app.use(methodOverRide("_method"));
 
 //Connecting our app to mongodb using mongoose
 main().then(()=>{
@@ -53,6 +55,30 @@ app.post("/chats",async (req,res)=>{
         to:receiver,
         created_at:new Date()
     });
+    res.redirect("/chats");
+});
+
+//Route to edit msg
+app.get("/chats/:id/edit",async (req,res)=>{
+    let {id}=req.params;
+    console.log("Edit request received");
+    let chat=await Chat.findById(id);
+    res.render("edit.ejs",{chat});
+});
+
+//Route to save the edited message
+app.put("/chats/:id",async (req,res)=>{
+    let {id}=req.params;
+    let {newmsg}=req.body;
+    await Chat.findByIdAndUpdate(id,{msg:newmsg});
+    res.redirect("/chats");
+});
+
+//Route to delete a chat
+app.delete("/chats/:id/delete",async (req,res)=>{
+    let {id}= req.params;
+    console.log(id);
+    await Chat.findByIdAndDelete(id);
     res.redirect("/chats");
 });
 
